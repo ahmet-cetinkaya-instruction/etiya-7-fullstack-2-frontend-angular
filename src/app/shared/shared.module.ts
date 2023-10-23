1a0b3b7a817f83f8d177ab496e8bd813a31c9d97
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-10-23 15:20:15 +0300

    add bootstrap and main layout component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MainLayoutComponent, NavbarComponent, FooterComponent],
+  exports: [MainLayoutComponent],
+  imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
